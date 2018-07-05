fde0a4fd00d386ee92b8dd53ae42062b6aa1b493
michellehkim <michellehkim@github.com>
2018-07-04 19:38:58 -0700

    edit after merge

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,12 +1,11 @@
 {
-<<<<<<< HEAD
+
     $('#album-title').text(album.title);
     $('img#album-cover-art').attr('src', album.albumArtUrl);
-=======
+
   $('#album-title').text(album.title);
   $("#artist").text(album.artist);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
 
   $("#release-info").text(album.releaseInfo);
->>>>>>> bloc-jams-song-list
 }
